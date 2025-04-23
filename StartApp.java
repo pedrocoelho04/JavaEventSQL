2460d99d27842440005b3741e87c4ebcfc280b87
sammykunimatsu <sammy.boy.br@gmail.com>
2025-04-23 00:51:29 -0300

    corrigido erro database

--- StartApp.java
+++ StartApp.java
@@ -1,32 +1,37 @@
-import java.util.List;
+// import java.util.List;
 
-import service.EventoService;
-import service.ParticipanteService;
-import table.Evento;
-import table.Participante;
+// import service.EventoService;
+// import service.ParticipanteService;
+// import table.Evento;
+// import table.Participante;
+
+// public class StartApp {
+//   public static void main(String[] args) {
+//     ParticipanteService ps = new ParticipanteService();
+//     List<Participante> listaParticipantes = ps.listarTodos();
+//     for (Participante item : listaParticipantes) {
+//       System.out.println("Nome: " + item.getNome() + "    E-mail: " + item.getEmail());
+//     }
+//     listaParticipantes = ps.listarPorParamentro("Vito", null);
+//     for (Participante item : listaParticipantes) {
+//       System.out.println("Nome: " + item.getNome() + "    Celular: " + item.getCelular());
+//     }
+//     Participante p = new Participante();
+//     p = ps.buscarPorId(1);
+//     System.out.println("Id: " + p.getId() + "   Nome: " + p.getNome());
+
+//     EventoService es = new EventoService();
+//     List<Evento> listaEventos = es.listarTodos();
+
+//     // System.out.println(ps.inserir("Ana", "F", "ana@gmail.com", "(27) 99734
+//     // 9870"));
+//   }
+// }
+
+import screens.HomeFrame;
 
 public class StartApp {
   public static void main(String[] args) {
-    ParticipanteService ps = new ParticipanteService();
-    List<Participante> listaParticipantes = ps.listarTodos();
-    for (Participante item : listaParticipantes) {
-      System.out.println("Nome: " + item.getNome() + "    E-mail: " + item.getEmail());
-    }
-    listaParticipantes = ps.listarPorParamentro("Vito", null);
-    for (Participante item : listaParticipantes) {
-      System.out.println("Nome: " + item.getNome() + "    Celular: " + item.getCelular());
-    }
-    Participante p = new Participante();
-    p = ps.buscarPorId(1);
-    System.out.println("Id: " + p.getId() + "   Nome: " + p.getNome());
-    ps.inserir("Pedro", "M", "p@email.com", "(27) 99999-9999", "S");
-
-    //ps.excluirPorId(3);
-
-    EventoService es = new EventoService();
-    List<Evento> listaEventos = es.listarTodos();
-
-    // System.out.println(ps.inserir("Ana", "F", "ana@gmail.com", "(27) 99734
-    // 9870"));
+    new HomeFrame();
   }
 }
